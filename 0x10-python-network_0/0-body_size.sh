852ef4d77492bb7c6bcd3fef96ec3c9877e22998
A.AJDIR <abdelaliajdir@gmail.com>
2024-03-28 07:06:45 -0700

    Update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the first argument
-url="$1"
-
-# Send a HEAD request to retrieve headers only (faster)
-response=$(curl -s -I "$url")
-
-# Extract the Content-Length header using grep
-content_length=$(echo "$response" | grep -i Content-Length | awk '{print $2}')
-
-# Check if Content-Length was found
-if [[ -z "$content_length" ]]; then
-  echo "Content-Length not found in response headers."
-  exit 1
-fi
-
-# Print the Content-Length
-echo "$content_length bytes"
+# Displays the size of the body of the response of a curl request
+curl -so /dev/null -w '%{size_download}\n' "$1"
